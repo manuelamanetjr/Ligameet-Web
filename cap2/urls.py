d71eb3125395ecf54ea1cfd00f52825e989806ee
Maning <82758432+manuelamanetjr@users.noreply.github.com>
2024-08-23 17:14:22 +0800

    profile picture added on views

--- cap2/urls.py
+++ cap2/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from users import views as user_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,3 +31,8 @@ urlpatterns = [
     path('', include('ligameet.urls')),
 
 ]
+
+if settings.DEBUG: #if debug in true
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
