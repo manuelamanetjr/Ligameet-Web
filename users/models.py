7d47563ef2a49e45f9a0b5d253e6bf45e135f8c1
Maning <82758432+manuelamanetjr@users.noreply.github.com>
2024-09-09 23:50:19 +0800

    models updated

--- users/models.py
+++ users/models.py
@@ -17,23 +17,23 @@ class Profile(models.Model):
         ('EVENT_ORG', 'Event Organizer'),
     ]
 
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=30, default='')
-    last_name = models.CharField(max_length=30, default='')
-    middle_name = models.CharField(max_length=30, blank=True, null=True)
-    date_of_birth = models.DateField(blank=True, null=True)
-    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, default='')
-    address = models.CharField(max_length=255, blank=True, null=True)
-    height = models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True)  # For storing height in meters
-    weight = models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True)  # For storing weight in kilograms
-    phone = models.CharField(max_length=15, blank=True, null=True)
-    email = models.EmailField(blank=True, null=True)
-    type = models.CharField(max_length=15, choices=TYPE_CHOICES, blank=True, null=True)
-    inv_code = models.CharField(max_length=50, blank=True, null=True)
-    is_coach = models.BooleanField(default=False)
-    is_athlete = models.BooleanField(default=False)
-    is_scout = models.BooleanField(default=False)
-    is_event_org = models.BooleanField(default=False)
+    USER = models.OneToOneField(User, on_delete=models.CASCADE)
+    FIRST_NAME = models.CharField(max_length=30, default='')
+    LAST_NAME = models.CharField(max_length=30, default='')
+    MIDDLE_NAME = models.CharField(max_length=30, blank=True, null=True)
+    DATE_OF_BIRTH = models.DateField(blank=True, null=True)
+    GENDER = models.CharField(max_length=1, choices=GENDER_CHOICES, default='')
+    ADDRESS = models.CharField(max_length=255, blank=True, null=True)
+    HEIGHT = models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True)  # For storing height in meters
+    WEIGHT = models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True)  # For storing weight in kilograms
+    PHONE = models.CharField(max_length=15, blank=True, null=True)
+    EMAIL = models.EmailField(blank=True, null=True)
+    TYPE = models.CharField(max_length=15, choices=TYPE_CHOICES, blank=True, null=True)
+    INV_CODE = models.CharField(max_length=50, blank=True, null=True)
+    IS_COACH = models.BooleanField(default=False)
+    IS_ATHLETE = models.BooleanField(default=False)
+    IS_SCOUT = models.BooleanField(default=False)
+    IS_EVENT_ORG = models.BooleanField(default=False)
     image = models.ImageField(default='user_default.png', upload_to='profile_pics')
 
     def __str__(self):
