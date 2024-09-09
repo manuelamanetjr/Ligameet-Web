803d9cd91c30ddc4957479c985610a164601e41c
Maning <82758432+manuelamanetjr@users.noreply.github.com>
2024-09-10 00:47:39 +0800

    models added to admin page

--- ligameet/admin.py
+++ ligameet/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Sport, Event, Participant, Wallet, File, Team, TeamParticipant
+from .models import Sport, Event, Participant, Wallet, File, Team, TeamParticipant, TeamEvent, Match, Subscription, TeamRegistrationFee, SportsEvent, TeamMatch, UserMatch, VolleyballStats, UserRegistrationFee, Payment, Transaction
 
 admin.site.register(Sport)
 admin.site.register(Event)
@@ -8,6 +8,16 @@ admin.site.register(Wallet)
 admin.site.register(File)
 admin.site.register(Team)
 admin.site.register(TeamParticipant)
-
+admin.site.register(TeamEvent)
+admin.site.register(Match)
+admin.site.register(Subscription)
+admin.site.register(TeamRegistrationFee)
+admin.site.register(SportsEvent)
+admin.site.register(TeamMatch)
+admin.site.register(UserMatch)
+admin.site.register(VolleyballStats)
+admin.site.register(UserRegistrationFee)
+admin.site.register(Payment)
+admin.site.register(Transaction)
 
 
