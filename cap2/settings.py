2ec7da1248aedfed13aa15e346a8a7f02c55b985
Maning <82758432+manuelamanetjr@users.noreply.github.com>
2024-09-14 01:09:49 +0800

    added custom admin-page

--- cap2/settings.py
+++ cap2/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'bootstrap4',
     'crispy_bootstrap4', 
+    # 'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
